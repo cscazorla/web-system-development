3e060731c2c271e8f90625094bfd674027315583
Carlos Sanchez Cazorla <cscazorla@moguplatform.com>
2025-04-16 14:18:40 +0200

    Added Mermaid for diagrams

--- slides/main.js
+++ slides/main.js
@@ -5,10 +5,14 @@ import './styles.css'
 import Reveal from 'reveal.js'
 import Highlight from 'reveal.js/plugin/highlight/highlight.esm.js'
 import 'reveal.js/plugin/highlight/monokai.css'
+import RevealMermaid from 'reveal.js-mermaid-plugin'
 
 const deck = new Reveal()
 deck.initialize({ 
-  plugins: [Highlight],
+  center: true,
+  margin: 0.01,
+  controlsLayout: 'bottom-right',
+  plugins: [Highlight, RevealMermaid],
   hash: true, 
   slideNumber: true,
   transition: 'slide'
