8ecca8f46d1c4e4992ae5128551a99568400059f
Carlos Sanchez Cazorla <cscazorla@moguplatform.com>
2025-06-21 09:44:33 +0200

    WIP

--- slides/main.js
+++ slides/main.js
@@ -1,19 +1,20 @@
-import 'reveal.js/dist/reveal.css'
-import 'reveal.js/dist/theme/black.css'
-import './styles.css'
+import Reveal from 'reveal.js';
+import 'reveal.js/dist/reveal.css';
+import 'reveal.js/dist/theme/black.css';
+import './styles.css';
 
-import Reveal from 'reveal.js'
-import Highlight from 'reveal.js/plugin/highlight/highlight.esm.js'
-import 'reveal.js/plugin/highlight/monokai.css'
-import RevealMermaid from 'reveal.js-mermaid-plugin'
+import RevealMermaid from 'reveal.js-mermaid-plugin';
 
-const deck = new Reveal()
-deck.initialize({ 
+import 'highlight.js/styles/atom-one-dark.css';
+import Highlight from 'reveal.js/plugin/highlight/highlight.esm.js';
+
+const deck = new Reveal();
+deck.initialize({
   center: true,
   margin: 0.05,
   controlsLayout: 'bottom-right',
   plugins: [Highlight, RevealMermaid],
-  hash: true, 
+  hash: true,
   slideNumber: true,
-  transition: 'slide'
-})
+  transition: 'slide',
+});
