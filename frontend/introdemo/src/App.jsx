36facdd06e7b913d027669cc34076aebaf647b93
Carlos Sanchez Cazorla <cscazorla@moguplatform.com>
2025-07-07 22:15:24 +0200

    WIP

---
+++ frontend/introdemo/src/App.jsx
@@ -0,0 +1,49 @@
+import './App.css';
+import Avatar from './Avatar.jsx';
+import Button from './Button.jsx';
+import CountersUpdatingTogether from './CountersUpdatingTogether.jsx';
+import ImprovedButtons from './ImprovedButtons.jsx';
+import ShoppingList from './ShoppingList.jsx';
+
+const Hello = (props) => {
+  console.log(props);
+
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear();
+    return yearNow - props.age;
+  };
+
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const name = 'Peter';
+  const age = 10;
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Avatar />
+      <ShoppingList />
+      <Button />
+      <Button />
+      <hr />
+      <CountersUpdatingTogether />
+      <hr />
+      <ImprovedButtons />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/introdemo/src/Button.jsx
@@ -0,0 +1,12 @@
+// Button.jsx
+import { useState } from 'react';
+
+export default function Button() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return <button onClick={handleClick}>Clicked {count} times</button>;
+}
